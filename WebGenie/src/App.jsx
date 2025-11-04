7ca2f74e432ead51f4763f043aad069c33b41938
Parul <parulkot2005@gmail.com>
2025-11-05 00:20:17 +0530

    solved tailwind css issue

--- WebGenie/src/App.jsx
+++ WebGenie/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+    </>
+    
   )
 }
 
